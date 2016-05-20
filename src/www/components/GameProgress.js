d6293037a7448181fd64dfa4699fccd707a0720a
giladartzi <gilad@artzi.org>
2016-05-20 08:56:56 +0200

    Minor improvements for GameProgress

--- src/www/components/GameProgress.js
+++ src/www/components/GameProgress.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import GameProgressBadge from './GameProgressBadge';
+import { NUM_OF_QUESTIONS } from '../../common/consts';
 
 let GameProgress = ({progress}) => {
-    let badges = progress.map((p, index) => {
-        return <GameProgressBadge isCurrent={index === progress.length-1} index={index} {...p} />;
+    let padded = [...progress];
+
+    while (padded.length < NUM_OF_QUESTIONS) {
+        padded.push({});
+    }
+
+    let badges = padded.map((p, index) => {
+        return <GameProgressBadge isCurrent={index === progress.length-1} index={index+1} {...p} />;
     });
 
     return (
--- src/www/components/GameProgressBadge.js
+++ src/www/components/GameProgressBadge.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Check from 'material-ui/svg-icons/navigation/check';
 import Clear from 'material-ui/svg-icons/content/clear';
+import Help from 'material-ui/svg-icons/action/help-outline';
+import Paper from 'material-ui/Paper';
 
 let GameProgressBadge = ({index, isAnswered, isCorrect, isCurrent}) => {
-    let style = {};
+    let style = {
+        height: 30,
+        width: 30,
+        margin: 5,
+        textAlign: 'center',
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    };
+
+    let content = <Help />;
     
-    if (isCurrent) {
-        style.textDecoration = 'underline';
-    }
-    else if (isAnswered) {
-        return isCorrect ? <Check /> : <Clear/>;
+    if (isAnswered) {
+        content = isCorrect ? <Check /> : <Clear/>;
     }
 
     return (
-        <span style={style}>{index}</span>
+        <Paper style={style} zDepth={2} circle={true}>{content}</Paper>
     );
 };
 
